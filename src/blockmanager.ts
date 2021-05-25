6cb1b02b0e22882d668126dff9f1046b0810a293
Jacob Zhang <jacob-z@live.de>
2021-05-26 00:34:43 +0200

    Block collision detection

--- src/blockmanager.ts
+++ src/blockmanager.ts
@@ -31,13 +31,19 @@ class BlockManager {
   }
 
   initBlock(block: Block) {
-    this._blocks.push(block);
+    // position
+    const coords = this.findFreeSpaceInContainer();
 
+    this._blocks.push(block);
     const blockTemplate = document.getElementById('block-template') as HTMLTemplateElement;
     const blockElement = blockTemplate.content.cloneNode(true) as HTMLElement;
     block.element = blockElement.querySelector('.block');
     block.initGrid();
     this._containerElement.appendChild(blockElement);
+
+    block.element.style.left = `${coords[0]}px`;
+    block.element.style.top = `${coords[1]}px`;
+
     const grid = block.element.querySelector('.grid');
     grid.addEventListener(
       'mouseover',
@@ -118,6 +124,45 @@ class BlockManager {
           block.toggleNote(event.target);
         }
       });
+  }
+
+  findFreeSpaceInContainer(): number[] {
+    if (this._blocks.every((b) => !b.element)) {
+      return [0, 0];
+    }
+    const containerWidth = this._containerElement.offsetWidth;
+    const containerHeight = this._containerElement.offsetHeight;
+    const blockWidth = this._blocks[0].element.offsetWidth;
+    const blockHeight = this._blocks[0].element.offsetHeight;
+    const margin = 16;
+
+    const maxPermittedX = containerWidth - blockWidth - margin;
+    const maxPermittedY = containerHeight - blockHeight - margin;
+
+    let coords = [0, 0];
+    let tries = 0;
+    do {
+      coords = [Math.random() * maxPermittedX, Math.random() * maxPermittedY];
+      tries += 1;
+    } while (this.anyBlockOccupying(coords[0], coords[1]) && tries < 100);
+
+    return coords;
+  }
+
+  anyBlockOccupying(x: number, y: number): boolean {
+    const blockWidth = this._blocks[0].element.offsetWidth;
+    const blockHeight = this._blocks[0].element.offsetHeight;
+
+    return this._blocks.some((b) => {
+      const blockX = b.element.style.left ? parseFloat(b.element.style.left) : 0;
+      const blockY = b.element.style.top ? parseFloat(b.element.style.top) : 0;
+      return (
+        blockX + blockWidth >= x &&
+        blockY + blockHeight >= y &&
+        blockY + blockHeight >= y &&
+        blockY <= y + blockHeight
+      );
+    });
   }
 }
 
